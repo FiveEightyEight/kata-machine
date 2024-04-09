29de57069d15f825e5a1d224b85affafc0195b1b
Robert Abreu <robert.abreu3@gmail.com>
2024-04-09 06:05:04 -0400

    binary search solution

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    // low
+    // high (exclusive)
+    if (haystack.length === 0) return false;
+    let low = 0;
+    let high = haystack.length;
+    do {
+        const mid_point = Math.floor(high - ((high - low) / 2));
+        const value = haystack[mid_point]
+        if (value === needle) return true
+       
+        if (value < needle) {
+            low = mid_point + 1;
+        } else if (value > needle) {
+            high = mid_point
+        }
+    } while (low < high);
+    return false;
 }
