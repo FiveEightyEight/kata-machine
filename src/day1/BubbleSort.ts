fe46ce8e763fc0a90649c77af7cf64c2aa7463f4
Robert Abreu <robert.abreu3@gmail.com>
2024-04-09 06:50:54 -0400

    bubble sort solution

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,19 @@
 export default function bubble_sort(arr: number[]): void {
+    // iterate
+    // each iteration check i+1
+    // if larger, swap (track swap)
+    // do till end
+    // next iteration do arr length - j
 
+    for (let i = 0, swap = false, j = arr.length; i < arr.length; ++i, --j) {
+        for (let k = 0; k < j; ++k) {
+            if (arr[k] > arr[k + 1]) {
+                let temp = arr[k+1]
+                arr[k+1] = arr[k]
+                arr[k] = temp
+                swap = true
+            }
+        }
+        if (!swap) break
+    }
 }
