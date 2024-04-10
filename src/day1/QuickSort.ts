8504780a45ec9fa29de796e4cea7964abb7195f0
Robert Abreu <robert.abreu3@gmail.com>
2024-04-10 10:16:28 -0400

    quick sort solution

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,30 @@
-export default function quick_sort(arr: number[]): void {
 
+const arr: number[] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) return;
+    const pivot = partition(arr, low, high)
+    qs(arr, low, (pivot - 1))
+    qs(arr, (pivot + 1), high)
+}
+
+function partition(arr: number[], low: number, high: number): number {
+    let pivot = arr[high]
+    let insert = low - 1; 
+    for (let i = low; i < high; ++i) {
+        if (arr[i] <= pivot) {
+            insert++;
+            const temp = arr[i];
+            arr[i] = arr[insert];
+            arr[insert] = temp;
+        }
+    }
+    insert++
+    arr[high] = arr[insert];
+    arr[insert] = pivot;
+    return insert;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length -1);
 }
