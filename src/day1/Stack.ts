d1ef8b99dd2a74e1b94c315baca8cb2e2213f596
Robert Abreu <robert.abreu3@gmail.com>
2024-04-10 05:56:46 -0400

    stack solution

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,37 @@
+type Node<T> = {
+    val: T,
+    next?: Node<T>,
+}
+
+
 export default class Stack<T> {
     public length: number;
+    private head: Node<T> | undefined;
 
-    
 
     constructor() {
+        this.length = 0
+        this.head = undefined;
     }
 
     push(item: T): void {
-
-}
+        const new_node = { val: item, next: undefined } as Node<T>;
+        if (!this.head) {
+            this.head = new_node;
+        } else {
+            new_node.next = this.head
+            this.head = new_node;
+        }
+        this.length++;
+    }
     pop(): T | undefined {
-
-}
+        if (!this.head) return;
+        const return_node = this.head;
+        this.head = return_node.next;
+        this.length--;
+        return return_node.val;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
